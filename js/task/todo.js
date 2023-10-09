d2ed100e6eac90069d80fd8e68f1aa396e677545
alexey <81881832+lesha2701@users.noreply.github.com>
2023-10-09 12:51:32 +0300

    123

--- js/task/todo.js
+++ js/task/todo.js
@@ -1,11 +1,23 @@
-let list = document.querySelector('.tasks-list');
-let dataList = []
-let id = 0
+let todoItems = [];
 
-document.forms.addTask.onsubmit = function() {
-    let task = this.task.value;
-    let createTask = document.createElement('li');
-    createTask.innerHTML(`${task}`)
-    dataList.push(`${task}`)
-    list.append(createTask);
+function addTask(text) {
+    const todo = {
+        text,
+        checked: false,
+        id: Date.now()
+    }
+
+    todoItems.push(todo);
 }
+
+const btn = document.querySelector('.create-task__button');
+btn.addEventListener('click', function() {
+    let input = document.getElementById('create-task__text');
+
+    if (input.value !== '') {
+        addTask(input.value);
+        console.log(todoItems);
+        input.value = '';
+    }
+})
+

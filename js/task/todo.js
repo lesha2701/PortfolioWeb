e18f80c01d4c11b47645e06ffd65be7f5430c48d
alexey <81881832+lesha2701@users.noreply.github.com>
2023-10-09 13:32:38 +0300

    123

--- js/task/todo.js
+++ js/task/todo.js
@@ -8,7 +8,43 @@ function addTask(text) {
     }
 
     todoItems.push(todo);
+    renderTodo(todo);   
 }
+
+function deleteTodo(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    const todo = {
+      deleted: true,
+      ...todoItems[index]
+    };
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    renderTodo(todo);
+  }
+
+function renderTodo(todo) {
+    const list = document.querySelector('.tasks-list');
+    const item = document.querySelector(`[data-key='${todo.id}']`);
+    const checked = todo.checked ? 'done' : '';
+
+    if (todo.deleted) {
+        item.remove();
+        return
+      }
+
+    const node = document.createElement('li');
+    node.setAttribute('class', `tasks-item ${checked}`);
+    node.setAttribute('data-key', todo.id);
+    node.innerHTML = `
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">X</button>
+    `
+    
+    if (item) {
+        list.replaceChild(node, item);
+    } else {
+        list.append(node);
+    }
+}   
 
 const btn = document.querySelector('.create-task__button');
 btn.addEventListener('click', function() {
@@ -21,3 +57,10 @@ btn.addEventListener('click', function() {
     }
 })
 
+const list = document.querySelector('.tasks-list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});

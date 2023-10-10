f86c8403356895e3c62d0eb419d90cdeb627ab63
alexey <81881832+lesha2701@users.noreply.github.com>
2023-10-10 17:45:02 +0300

    123

---
+++ js/colorGenerator/app.js
@@ -0,0 +1,78 @@
+const cols = document.querySelectorAll('.col');
+
+// function gerenerateRandomColor() {
+//     const hexCodes = '0123456789ABCDEF';
+//     let color = ''
+
+//     for (let i = 0; i < 6; i++) {
+//         color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+//     }
+
+//     return '#' + color
+// }
+
+function copyToClockColor(text) {
+    navigator.clipboard.writeText(text)
+}
+
+document.addEventListener('click', event => {
+    const type = event.target.dataset.type
+    if (type === 'lock') {
+        const node = event.target.tagName.toLowerCase() === 'i'
+        ? event.target : event.target.children[0]
+
+        console.log(node)
+        node.classList.toggle('fa-unlock')
+        node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        copyToClockColor(event.target.innerText)
+    }
+
+})
+
+function setRandomColors() {
+    const colors = []
+
+    cols.forEach(col => {
+        const isLock = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = chroma.random()
+
+
+        if (isLock) {
+            colors.push(text.textContent)
+            return  
+        }
+
+        colors.push(color)
+
+        col.style.background = color
+        text.textContent = color    
+
+        setTextColor(button, color)
+        setTextColor(text, color)
+    })
+    console.log(colors)
+    updateColorsHash(colors)
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+document.addEventListener('keydown', event => {
+    event.preventDefault()
+    if (event.code === 'Space') {
+        setRandomColors()
+    }
+})
+
+function updateColorsHash(colors = []) {
+    document.location.hash = colors.map(col => {return col.toString()
+    .substring(1)})
+    .join('-')
+}
+
+setRandomColors()
